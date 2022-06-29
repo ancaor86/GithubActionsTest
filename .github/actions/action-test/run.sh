e39df05d04bd59adf163b60b93767c7c104d1731
Andrés Carmona <andres.carort@gmail.com>
2022-06-29 12:56:41 -0500

    Added action to test

---
+++ .github/actions/action-test/run.sh
@@ -0,0 +1,15 @@
+if [[ $GLOBAL == "Global Environment"  ]]
+then
+echo $GLOBAL
+echo ${{ inputs.os-name }}
+fi
+
+STATUS='"UPLOADING"'
+while [ "$STATUS" != '"SUCCEEDED"' ] 
+do
+echo "UPLOADING..."
+STATUS='"SUCCEEDED"'
+echo STATUS
+done
+
+echo "::set-output name=action-output::$STATUS"
